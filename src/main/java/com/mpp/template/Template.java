77bffe1905c62b9a8575f1ea2beeeb6ee853eceb
gchdljsygch <47450941+gchdljsygch@users.noreply.github.com>
2026-09-19 17:07:05 +0800

    init

---
+++ src/main/java/com/mpp/template/Template.java
@@ -0,0 +1,13 @@
+package com.pingsu.template;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(Template.MODID)
+public class Template {
+
+    public static final String MODID = "template";
+
+    public Template() {}
+
+}
